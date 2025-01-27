5318af3c024c915903afc5c3f2e7d170be1ae622
Leo Tamminen <lettam@utu.fi>
2025-01-27 17:22:31 +0200

    MainLayout.tsx test
    Changed to same gradient as general to see if it works.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="layout flex flex-col min-h-screen w-full bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 text-white">
+    <div className="layout flex flex-col min-h-screen w-full bg-gradient-to-r from-blue-800 via-cyan-700 to-blue-500 text-white dark:from-gray-900 dark:via-black dark:to-blue-900 dark:text-gray-200">
       {/* Navbar */}
       <header className="bg-gray-800 text-white w-full">
         <Navbar />
